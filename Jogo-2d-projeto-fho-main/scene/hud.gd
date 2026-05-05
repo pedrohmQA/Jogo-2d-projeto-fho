bdebbd68c634b387ea995e17d754059c5c88973b
pedrohmQA <moraespedro161@gmail.com>
2026-05-05 16:18:54 -0300

    organiza estrutura das pastas na nova versão

--- Jogo-2d-projeto-fho-main/scene/hud.gd
+++
@@ -1,6 +0,0 @@
-extends CanvasLayer
-
-@onready var garbage_label: Label = $TopRight/MarginContainer/Counters/GarbageCounter/GarbageLabel
-
-func _process(_delta: float) -> void:
-	garbage_label.text = "x" + str(QuestState.garbage_left)
--- Jogo-2d-projeto-fho-main/scripts/hud_forest.gd
+++
@@ -1,8 +0,0 @@
-extends CanvasLayer
-
-@onready var apple_label: Label = $TopRight/MarginContainer/Counters/AppleLabel
-@onready var coin_label: Label = $TopRight/MarginContainer/Counters/CoinLabel
-
-func _process(_delta: float) -> void:
-	apple_label.text = "x" + str(QuestState.apples)
-	coin_label.text = "x" + str(QuestState.coins)
